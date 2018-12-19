c98d3a6f0c13ecf45209d507a868e7f026c0bf68
Sander <sander@funkhaus.us>
2018-12-19 12:02:44 -0500

    Adding resizing canvas

--- src/ThreeView.vue
+++ src/ThreeView.vue
@@ -1,12 +1,45 @@
 <template>
-    <main class="class"></main>
+    <div class="three-view">
+        <canvas :width="cmpWidth" :height="cmpHeight" />
+    </div>
 </template>
 
 <script>
-export default {}
+import rect from 'fh-components/mixins/rect'
+
+export default {
+    mixins: [rect],
+    props: {
+        width: {
+            type: Number,
+            default: -1
+        },
+        height: {
+            type: Number,
+            default: -1
+        }
+    },
+    computed: {
+        cmpWidth() {
+            if (this.width != -1) return this.width
+            return this.clientRect ? this.clientRect.width : 0
+        },
+        cmpHeight() {
+            if (this.height != -1) return this.height
+            return this.clientRect ? this.clientRect.height : 0
+        }
+    }
+}
 </script>
 
 <style lang="scss">
-.class {
+.three-view {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: black;
+    font-size: 0;
 }
 </style>
