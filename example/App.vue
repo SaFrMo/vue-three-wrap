1b0abb17fbd82e9cec44ee313a4da3436250b8d1
Sander <sander@funkhaus.us>
2018-12-19 14:29:07 -0500

    Added example scene

---
+++ example/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <main class="example"><three-view :start="start" :update="update" /></main>
+</template>
+
+<script>
+/* eslint-disable */
+import ThreeView from '../src/ThreeView'
+import * as THREE from 'three'
+
+let cube
+
+export default {
+    components: {
+        'three-view': ThreeView
+    },
+    methods: {
+        start({ scene, camera, renderer }) {
+            // example adding a cube to the scene
+            const geometry = new THREE.BoxGeometry(1, 1, 1)
+            const material = new THREE.MeshBasicMaterial({ color: 0x000000 })
+            cube = new THREE.Mesh(geometry, material)
+            scene.add(cube)
+            camera.position.z = 10
+        },
+        update({ scene, camera, renderer }) {
+            cube.rotation.y -= 0.01
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.example {
+}
+</style>
--- src/ThreeView.vue
+++ src/ThreeView.vue
@@ -19,6 +19,10 @@ export default {
             type: Number,
             default: -1
         },
+        start: {
+            type: Function,
+            default: null
+        },
         update: {
             type: Function,
             default: null
@@ -27,9 +31,13 @@ export default {
             type: Object,
             default: () => new THREE.PerspectiveCamera(75, 0.5625, 0.1, 1000)
         },
-        renderer: {
+        rendererOptions: {
             type: Object,
             default: () => {}
+        },
+        renderLoop: {
+            type: Boolean,
+            default: true
         }
     },
     data() {
@@ -38,54 +46,62 @@ export default {
         }
     },
     mounted() {
+        // create scene, camera, and renderer
         this.three.scene = new THREE.Scene()
-
         this.three.camera = this.camera
-
         this.three.renderer = new THREE.WebGLRenderer({
             canvas: this.$refs.canvas,
             antialias: true,
             alpha: true,
-            ...this.renderer
+            ...this.rendererOptions
         })
-        this.three.renderer.setSize(this.cmpWidth, this.cmpHeight)
 
-        // example adding a cube to the scene
-        // var geometry = new THREE.BoxGeometry(1, 1, 1)
-        // var material = new THREE.MeshBasicMaterial({ color: 0x000000 })
-        // var cube = new THREE.Mesh(geometry, material)
-        // this.three.scene.add(cube)
-        // this.three.camera.position.z = 10
+        // update camera
+        this.updateCamera()
 
-        var animate = () => {
-            requestAnimationFrame(animate)
+        // run start function if one provided
+        if (this.start) {
+            this.start(this.three)
+        }
+
+        // kick animation
+        this.render()
+    },
+    computed: {
+        cmpWidth() {
+            // use width prop if provided
+            if (this.width != -1) return this.width
+            // fall back to client rect size or 0
+            return this.clientRect ? this.clientRect.width : 0
+        },
+        cmpHeight() {
+            // use height prop if provided
+            if (this.height != -1) return this.height
+            // fall back to client rect size or 0
+            return this.clientRect ? this.clientRect.height : 0
+        }
+    },
+    methods: {
+        updateCamera() {
+            // ignore if no camera
+            if (!this.three.camera) return
+
+            // update aspect ratio, projection matrix, and renderer size
+            this.three.camera.aspect = this.cmpWidth / this.cmpHeight
+            this.three.camera.updateProjectionMatrix()
+            this.three.renderer.setSize(this.cmpWidth, this.cmpHeight)
+        },
+        render() {
+            // request next frame
+            if (this.renderLoop) {
+                requestAnimationFrame(this.render)
+            }
 
             if (this.update) {
                 this.update(this.three)
             }
 
             this.three.renderer.render(this.three.scene, this.three.camera)
-        }
-
-        animate()
-    },
-    computed: {
-        cmpWidth() {
-            if (this.width != -1) return this.width
-            return this.clientRect ? this.clientRect.width : 0
-        },
-        cmpHeight() {
-            if (this.height != -1) return this.height
-            return this.clientRect ? this.clientRect.height : 0
-        }
-    },
-    methods: {
-        updateCamera() {
-            if (!this.three.camera) return
-
-            this.three.camera.aspect = this.cmpWidth / this.cmpHeight
-            this.three.camera.updateProjectionMatrix()
-            this.three.renderer.setSize(this.cmpWidth, this.cmpHeight)
         }
     },
     watch: {
