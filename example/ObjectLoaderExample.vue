5f7e235ec6ee173d71b0f69f3dca245874fa0757
Sander Moolin <sandermoolin@gmail.com>
2019-09-25 09:12:22 -0400

    Loader working and example ready

--- example/ObjectLoaderExample.vue
+++ example/ObjectLoaderExample.vue
@@ -1,11 +1,16 @@
 <template>
-    <vue-three-wrap class="fragment-shader" :start="start" :update="update" />
+    <vue-three-wrap class="object-loader" :start="start">
+        <p class="info">
+            This is a <code>.glb</code> object exported from Blender and loaded
+            using the <code>load-gltf</code> extra.
+        </p>
+    </vue-three-wrap>
 </template>
 
 <script>
 import VueThreeWrap from '../src/VueThreeWrap'
 import * as THREE from 'three'
-import loadGltf from '../src/extras/load-gltf/'
+import { loadObjects, default as loadScene } from '../src/extras/load-gltf/'
 
 const ref = {}
 
@@ -15,24 +20,53 @@ export default {
     },
     methods: {
         async start({ scene, camera, vertexShader, fragmentShader }) {
-            // const geo = new THREE.BoxGeometry()
-            // const mat = new THREE.MeshBasicMaterial({ color: 0xabd66a })
-            // ref.cube = new THREE.Mesh(geo, mat)
-            // scene.add(ref.cube)
-            const model = await loadGltf('/assets/scene.glb')
-            scene.add(model)
+            this.prepLighting(scene)
 
-            camera.position.set(2, 1, 2)
+            // you can load a full scene...
+            // const glb = await loadScene('/assets/scene.glb')
+            // ref.scene = glb.scene
+            // scene.add(ref.scene)
+
+            // ...or individual objects
+            ref.objects = await loadObjects('/assets/scene.glb')
+            ref.objects.forEach(obj => scene.add(obj))
+
+            camera.position.set(-4, 4, 4)
             camera.lookAt(new THREE.Vector3(0, 0, 0))
         },
-        update() {
-            // ref.cube.rotation.y -= 0.001
+        prepLighting(scene) {
+            // Ambient light
+            ref.light = new THREE.AmbientLight(0xffffff, 0.5)
+
+            // Shadow light
+            ref.shadowLight = new THREE.DirectionalLight(0xffffff, 0.5)
+            ref.shadowLight.position.set(200, 200, 200)
+            ref.shadowLight.castShadow = true
+            ref.shadowLight.shadow.mapSize.width = ref.shadowLight.shadow.mapSize.height = 2048
+
+            // Backlight
+            ref.backLight = new THREE.DirectionalLight(0xffffff, 0.2)
+            ref.backLight.position.set(-100, 200, 50)
+            // ref.backLight.castShadow = true
+
+            // Add lights
+            scene.add(ref.backLight)
+            scene.add(ref.light)
+            scene.add(ref.shadowLight)
         }
     }
 }
 </script>
 
 <style lang="scss">
-.fragment-shader {
+.object-loader {
+    .info {
+        font-size: 14px;
+        position: absolute;
+        right: 20px;
+        bottom: 0;
+        left: 20px;
+        background: white;
+    }
 }
 </style>
--- src/extras/load-gltf/index.js
+++ src/extras/load-gltf/index.js
@@ -3,8 +3,23 @@ import GLTFLoader from './gltf-loader'
 const loader = new GLTFLoader()
 
 export default path => {
-    console.log(loader)
     return new Promise((res, rej) => {
         loader.load(path, gltf => res(gltf), undefined, err => rej(err))
+    })
+}
+export const loadObjects = path => {
+    return new Promise((res, rej) => {
+        loader.load(
+            path,
+            gltf => {
+                res(
+                    gltf && gltf.scene && gltf.scene.children
+                        ? gltf.scene.children
+                        : null
+                )
+            },
+            undefined,
+            err => rej(err)
+        )
     })
 }
